72d6441d5727fa6528a7a631f326d3e493f8afbc
Mechanised Mango Box <mechanised_mango_box@proton.me>
2025-11-30 01:08:55 +1100

    Instruction defines args

--- asm-prototype/test.py
+++ asm-prototype/test.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Any, Dict, List, Literal, Tuple, TypeVar
+from typing import Any, Dict, List, Literal, Never
 
 # RegisterName = str
 # Value = ImmediateValue | Collection[ImmediateValue]
@@ -10,10 +10,12 @@ Opcode = Literal["SUM", "PRODUCT"]
 
 Value = Any
 
+
 @dataclass(frozen=True)
 class Variable:
     tag: Type
     value: Value
+
 
 VariableName = str
 Arg = VariableName | Value
@@ -21,37 +23,52 @@ Arg = VariableName | Value
 
 @dataclass(frozen=True)
 class Instruction:
-    args: Tuple[Arg, ...]
+    pass
 
 
 @dataclass(frozen=True)
 class Sum(Instruction):
-    args: Tuple[VariableName, Arg, Arg]
+    destination: VariableName
+    source_1: Arg
+    source_2: Arg
 
 
 @dataclass(frozen=True)
 class Product(Instruction):
-    args: Tuple[VariableName, Arg, Arg]
+    destination: VariableName
+    source_1: Arg
+    source_2: Arg
 
 
 class Machine:
-    variables: Dict[str, Variable]
-    instructions: List[Instruction]
+    variables: Dict[str, Variable] = {}
+    instructions: List[Instruction] = []
+
+    instruction_index: int = 0
 
     def run(self):
-        lineIndex: int = 0
-        while True: # TODO
-            instruction:Instruction = self.instructions[lineIndex] 
+        while True:  # TODO
+            instruction: Instruction = self.instructions[self.instruction_index]
             self.execute(instruction)
 
     def execute(self, instruction: Instruction):
         match instruction:
             case Sum():
                 # TODO checks
-                res = instruction.args[1] + instruction.args[2]
-                writeToVarName:VariableName = instruction.args[0]
-                self.variables[writeToVarName] = 
+                self.variables[instruction.destination] = self.get_value(
+                    instruction.source_1) * self.get_value(instruction.source_2)
+                self.increment_instruction()
 
+            case Product():
+                # TODO checks
+                self.variables[instruction.destination] = self.get_value(
+                    instruction.source_1) * self.get_value(instruction.source_2)
+                self.increment_instruction()
 
-def foo():
-    s: Sum = Sum(("myvar1", "myvar2", 134))
+            case Instruction(): assert (Never)  # This should never happen
+
+    def get_value(self, variable_name: VariableName) -> Value:
+        return self.variables[variable_name]
+
+    def increment_instruction(self):
+        self.instruction_index += 1
